9572e31d6e9147dabbb6da9171281d698df5a021
qixian0622 <97826360+qixian0622@users.noreply.github.com>
2023-07-03 23:49:52 +0800

    add some content to timetable

---
+++ Jr_Academy/screens/Teacher/TimeTable/TimeTable.js
@@ -0,0 +1,13 @@
+import React, { useState, useLayoutEffect } from 'react';
+import { View, TouchableOpacity, TextInput, StyleSheet, Text, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const TimeTable = () => {
+    return (
+        <View>
+            <Text>Hi timetable screen</Text>
+        </View>
+    );
+};
+
+export default TimeTable;
