56afca7f61c872f61020d787891a714c41837dcd
rxvmah001 <rxvmah001@sim.edu.sg>
2023-07-05 14:43:54 +0800

    update teacherpage

--- Jr_Academy/screens/Teacher/Teacherpage.js
+++ Jr_Academy/screens/Teacher/Teacherpage.js
@@ -3,6 +3,7 @@ import { SafeAreaView, StyleSheet, View, Text, Button} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 
 //import from the respective file path
 import HomePageScreen from './Homepage/HomePage';
@@ -17,16 +18,32 @@ const Teacherpage= ({}) => {
 
       <NavigationContainer>
         <Tab.Navigator screenOptions={{headerShown:false}}>
-          <Tab.Screen name="Attendance" component={AttendanceScreen} />
-          <Tab.Screen name="TimeTable" component={TimeTableScreen} />
+          <Tab.Screen name="Attendance" component={AttendanceScreen} options={{
+            tabBarIcon: () => (
+              <Ionicons name="checkbox-outline" size={24} color="black" />
+            ),
+          }}/>
+          <Tab.Screen name="TimeTable" component={TimeTableScreen} options={{
+            tabBarIcon: () => (
+              <Ionicons name="calendar-outline" size={24} color="black" />
+            ),
+          }}/>
           <Tab.Screen name="Home" component={HomePageScreen} options={{
                         tabBarIcon: () => (
                           <Ionicons name="home" size={24}/>
                         ),
           }}/>          
             
-            <Tab.Screen name="Alerts" component={AlertScreen} />
-          <Tab.Screen name="Setting" component={SettingsScreen} />        
+            <Tab.Screen name="Alerts" component={AlertScreen} options={{
+            tabBarIcon: () => (
+              <FontAwesome name="bell-o" size={24} color="black" />
+            ),
+          }}/>
+          <Tab.Screen name="Setting" component={SettingsScreen} options={{
+            tabBarIcon: () => (
+              <Ionicons name="settings-outline" size={24} color="black" />
+            ),
+          }}/>        
         </Tab.Navigator>
     </NavigationContainer>   
 
