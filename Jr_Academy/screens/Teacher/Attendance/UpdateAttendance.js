92163308e46b45e7bc80192ff78a075157ed8194
qixian0622 <97826360+qixian0622@users.noreply.github.com>
2023-07-07 21:13:37 +0800

    test content for update attendance page

---
+++ Jr_Academy/screens/Teacher/Attendance/UpdateAttendance.js
@@ -0,0 +1,21 @@
+import React, { useState, useLayoutEffect } from 'react';
+import { View, TouchableOpacity, Switch, StyleSheet, Text, Button } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+
+const UpdateAttendance = () => {
+    return (
+        <View>
+            <Text>this is update attendance page, wohooooo testing</Text>
+        </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+
+
+  });
+  
+export default UpdateAttendance;
