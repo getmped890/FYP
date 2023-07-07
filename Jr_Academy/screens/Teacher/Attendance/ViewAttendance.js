8efe73ffdbda2e2c348813ad11d24afa8e22d34d
qixian0622 <97826360+qixian0622@users.noreply.github.com>
2023-07-07 21:13:53 +0800

    done for view attendance page

---
+++ Jr_Academy/screens/Teacher/Attendance/ViewAttendance.js
@@ -0,0 +1,21 @@
+import React, { useState, useLayoutEffect } from 'react';
+import { View, TouchableOpacity, Switch, StyleSheet, Text, Button } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+
+const ViewAttendance = () => {
+    return (
+        <View>
+            <Text>this is view attendance page, wohooooo testing</Text>
+        </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+
+
+  });
+  
+export default ViewAttendance;
