b4595cfbf309dec96c1a888c4108c75d58b58914
getmped890 <yueloong678@hotmail.com>
2023-07-09 14:53:16 +0800

    Login Page (Partically Done)

--- Jr_Academy/App.js
+++ Jr_Academy/App.js
@@ -2,13 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Teacherpage from './screens/Teacher/Teacherpage';
 import Childpage from './screens/Child/Childpage';
+import Login from './screens/Login/Login';
 
 
 export default function App() {
   
   return (
     <View style={styles.container}>
-      <Teacherpage />
+      <Login/>
     </View>
   );
 }
