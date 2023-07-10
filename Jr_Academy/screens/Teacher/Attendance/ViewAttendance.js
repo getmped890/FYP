4c545a33027a02600bbc7c947a3bd66d00a6c921
hidayah <wooozy.clouds@gmail.com>
2023-07-10 10:08:04 +0800

    added viewattendance page

--- Jr_Academy/screens/Teacher/Attendance/ViewAttendance.js
+++ Jr_Academy/screens/Teacher/Attendance/ViewAttendance.js
@@ -3,18 +3,93 @@ import { View, TouchableOpacity, Switch, StyleSheet, Text, Button } from 'react-
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
+const persons = [
+    {
+        id: "1",
+        name: "Earnest Green",
+        status: "present",
+    },
+
+
+    {
+        id: "2",
+        name: "Winston Orn",
+        status: "absent",
+    },
+
+    {
+        id: "3",
+        name: "Carlton Collins",
+        status: "late",
+    },
+
+    {
+        id: "4",
+        name: "Malcolm Labadie",
+        status: "present",
+    }
+];
+
 
 const ViewAttendance = () => {
+
+    const myItemSeparator = () => {
+        return <View style={{ height: 1, backgroundColor: "grey",marginHorizontal:10}} />;
+        };
+      
+      const myListEmpty = () => {
+        return (
+          <View style={{ alignItems: "center" }}>
+          <Text style={styles.item}>No data found</Text>
+          </View>
+        );
+      };
+    
+    
+      const [selected, setSelected] = React.useState("");
+      
+    
     return (
-        <View>
-            <Text>this is view attendance page, wohooooo testing</Text>
-        </View>
+
+  <View style={styles.container}>
+
+  
+    <FlatList
+      data={persons}
+      renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+      keyExtractor={(item) => item.id}
+      ItemSeparatorComponent={myItemSeparator}
+      ListEmptyComponent={myListEmpty}
+      ListHeaderComponent={() => (
+        <Text style={{ fontSize: 20, textAlign: "center",marginTop:20,fontWeight:'bold' }}>
+          Attendance for today:
+        </Text>
+
+        
+ 
+      )}/>
+
+
+   </View> 
+
+
     );
 };
 
 
 const styles = StyleSheet.create({
 
+    container: {
+        flex: 1,
+        marginTop: 5,
+        fontSize: 30,
+      },
+      item: {
+        padding: 20,
+        marginTop: 5,
+        fontSize: 15,
+        textAlign: 'center',
+      },
 
   });
   
