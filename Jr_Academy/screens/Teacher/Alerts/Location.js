f760ba39cbd10a0dc87cde104d6236104444cd58
Krars <frankieongmeh@gmail.com>
2023-07-10 16:17:27 +0800

    Update

--- Jr_Academy/screens/Teacher/Alerts/Location.js
+++ Jr_Academy/screens/Teacher/Alerts/Location.js
@@ -8,20 +8,42 @@ const Location = () => {
      //use for navigating/redirect to other page
   const navigation = useNavigation();
 
-  const checkGPS = () => {
+  const BackAlert = () => {
     //navigate to GPS Page
     navigation.navigate('Alerts');
   };
 
  return (
     <View>
-        <Text>
-        Button Return
-        </Text>
+       <Text style={styles.backText}>Back</Text>       
+        <TouchableOpacity style={styles.backButton}>
+        <Ionicons name="arrow-undo-sharp" size={35} onPress={BackAlert}/>
+      </TouchableOpacity>
     </View>
- );
+ );};
 
 
-};
+ const styles = StyleSheet.create({
 
+
+    backButton: {
+
+        position: 'absolute',
+        top: 80,
+        left: 30,
+        zIndex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+
+    backText:{
+        position: 'relative',
+        top:50,
+        left:30,
+        fontSize:15,
+      fontWeight: 'bold',
+    },
+  
+});
+  
 export default Location;
