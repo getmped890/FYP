ae534782872d98ce24a11d465da7ea754d0f3a34
qixian0622 <97826360+qixian0622@users.noreply.github.com>
2023-07-10 20:07:27 +0800

    fix homepage eror

--- Jr_Academy/screens/Teacher/Homepage/HomePage.js
+++ Jr_Academy/screens/Teacher/Homepage/HomePage.js
@@ -1,164 +1,151 @@
 import React from 'react';
-import { View, Text, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, TouchableOpacity, Dimensions, Alert } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-
-import SettingsScreen from '../Setting/Setting';
-
 const HomePage = () => {
   //use for navigating/redirect to other page
   const navigation = useNavigation();
-
   const handleUserIconClick = () => {
     //navigate to setting page
-    navigation.navigate('Setting');
+    navigation.navigate('Profile');
   };
-
+  const handleEndClass = () => {
+    Alert.alert('Class Ended!');
+  };
   return (
     <View style={styles.container}>
-      <View style={styles.topLeft}>
-        <Text style={styles.welcometext}>Jason</Text>
+      <View style={styles.background}>
+        <View style={styles.topLeft}>
+          <Text style={styles.welcomeText}>Jason</Text>
+        </View>
+        <TouchableOpacity style={styles.topRight} onPress={handleUserIconClick}>
+          <Ionicons name="person-outline" size={35} color="#FFFFFF" />
+        </TouchableOpacity>
+        <View style={styles.scrollContainer}>
+          <Text style={styles.headerText}>Upcoming Classes:</Text>
+          <ScrollView horizontal={true} contentContainerStyle={styles.scrollContent}>
+            <View style={[styles.card, styles.cardElevated]}>
+              <Text style={styles.classText}>CSIT203</Text>
+              <Text style={styles.classText}>26th Jun 2023</Text>
+              <Text style={styles.classText}>3:30pm - 6:30pm</Text>
+            </View>
+            <View style={[styles.card, styles.cardElevated]}>
+              <Text style={styles.classText}>CSIT503</Text>
+              <Text style={styles.classText}>20th Jun 2023</Text>
+              <Text style={styles.classText}>2:30pm - 4:30pm</Text>
+            </View>
+            <View style={[styles.card, styles.cardElevated]}>
+              <Text style={styles.classText}>CSIT123</Text>
+              <Text style={styles.classText}>29th Jun 2023</Text>
+              <Text style={styles.classText}>3:30pm - 6:30pm</Text>
+            </View>
+          </ScrollView>
+        </View>
+        <View style={styles.absentContainer}>
+          <Text style={styles.headerText}>Absentees Based On Last Class:</Text>
+          <ScrollView horizontal={true} contentContainerStyle={styles.scrollContent}>
+            <View style={[styles.card, styles.cardElevated]}>
+              <Text style={styles.classText}>1. Wong Kai Yih</Text>
+            </View>
+            <View style={[styles.card, styles.cardElevated]}>
+              <Text style={styles.classText}>2. Navindran a/l duraisingam</Text>
+            </View>
+            <View style={[styles.card, styles.cardElevated]}>
+              <Text style={styles.classText}>3. Chow Xuhua</Text>
+            </View>
+          </ScrollView>
+        </View>
+        <TouchableOpacity style={styles.endClassButton} onPress={handleEndClass}>
+          <Ionicons name="checkmark" size={35} color="#1DC1B1" />
+        </TouchableOpacity>
+        <Text style={styles.endClassText}>End Class</Text>
       </View>
-
-      <TouchableOpacity style={styles.topRight} onPress={handleUserIconClick}>
-        <Ionicons name="person-outline" size={35} color="black" />
-      </TouchableOpacity>
-    
-      <View style={styles.scrollContainer}>
-        <Text style={styles.headertext}>Upcoming Classes:</Text>
-        <ScrollView horizontal={true} contentContainerStyle={styles.scrollContent}>
-        <View style={[styles.card, styles.cardElevated]}>
-            <Text style={styles.classText}>CSIT203</Text>
-            <Text style={styles.classText}>26th Jun 2023</Text>
-            <Text style={styles.classText}>3:30pm - 6:30pm</Text>
-          </View>
-          <View style={[styles.card, styles.cardElevated]}>
-            <Text style={styles.classText}>CSIT503</Text>
-            <Text style={styles.classText}>20th Jun 2023</Text>
-            <Text style={styles.classText}>2:30pm - 4:30pm</Text>
-          </View>
-          <View style={[styles.card, styles.cardElevated]}>
-            <Text style={styles.classText}>CSIT123</Text>
-            <Text style={styles.classText}>29th Jun 2023</Text>
-            <Text style={styles.classText}>3:30pm - 6:30pm</Text>
-          </View>
-        </ScrollView>
-      </View>
-
-      <View style={styles.absentContainer}>
-        <Text style={styles.headertext}>Absentees Based On Last Class:</Text>
-        <ScrollView horizontal={true} contentContainerStyle={styles.scrollContent}>
-        <View style={[styles.card, styles.cardElevated]}>
-            <Text style={styles.classText}>1. Wong Kai Yih</Text>
-          </View>
-        <View style={[styles.card, styles.cardElevated]}>
-            <Text style={styles.classText}>2. Navindran a/l duraisingam</Text>
-          </View>
-        <View style={[styles.card, styles.cardElevated]}>
-            <Text style={styles.classText}>3. Chow Xuhua</Text>
-          </View>
-        </ScrollView>
-      </View>
-
-      <TouchableOpacity style={styles.endClassButton}>
-        <Ionicons name="checkmark" size={35} />
-      </TouchableOpacity>
-      <Text style={styles.endClassText}>End Class</Text>
     </View>
   );
 };
-
+const { width, height } = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    position: 'relative',
+  },
+  background: {
+    flex: 1,
+    backgroundColor: '#B3EAE5',
   },
   topLeft: {
-    position: 'relative',
-    top: 50,
-    left: 5,
+    position: 'absolute',
+    top: height * 0.08,
+    left: width * 0.05,
   },
   topRight: {
     position: 'absolute',
-    top: 40,
-    right: 10,
+    top: height * 0.07,
+    right: width * 0.05,
     zIndex: 1,
   },
-
-  card:{
+  card: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    width: 250,
-    height: 120,
+    width: width * 0.65,
+    height: height * 0.16,
+    width: width * 0.60,
+    height: height * 0.15,
     borderRadius: 4,
-    margin: 25
+    margin: width * 0.05,
+    backgroundColor: '#FFFFFF',
   },
-  cardElevated:{
-    backgroundColor: 'white',
+  cardElevated: {
     elevation: 4,
     shadowOffset: {
-        width: 1,
-        height: 1
-      },
+      width: 1,
+      height: 1,
+    },
   },
   scrollContainer: {
-    paddingTop: 150,
+    paddingTop: height * 0.2,
   },
-
   absentContainer: {
-    paddingTop: 50,
+    paddingTop: height * 0.03,
   },
-
- 
-  welcometext: {
-    fontSize: 20,
+  welcomeText: {
+    fontSize: height * 0.03,
     fontWeight: 'bold',
-    fontStyle:'italic',
-    fontFamily: 'Arial', 
+    fontStyle: 'italic',
+    color: '#FFFFFF',
   },
-
-  headertext: {
-    fontSize: 20,
+  headerText: {
+    fontSize: height * 0.03,
     fontWeight: 'bold',
-    marginBottom: 10,
-    paddingHorizontal: 8
+    marginBottom: height * 0.01,
+    paddingHorizontal: width * 0.05,
+    color: '#FFFFFF',
   },
   classText: {
-    fontSize: 16,
+    fontSize: height * 0.02,
+    color: '#1DC1B1',
   },
   scrollContent: {
     alignItems: 'center',
   },
-  classItem: {
-    width: 250,
-    height: 120,
-    marginRight: 10,
-    backgroundColor: 'lightgray',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   endClassButton: {
     position: 'absolute',
-    bottom: 30,
-    right: 20,
-    width: 80,
-    height: 80,
-    borderRadius: 50,
-    backgroundColor: 'white',
+    bottom: height * 0.05,
+    right: width * 0.03,
+    width: width * 0.2,
+    height: width * 0.14,
+    borderRadius: width * 0.1,
+    backgroundColor: '#FFFFFF',
     justifyContent: 'center',
     alignItems: 'center',
   },
-
-  endClassText:{
+  endClassText: {
     position: 'absolute',
-    bottom:8,
-    right:25,
+    bottom: height * 0.01,
+    right: width * 0.03,
     fontWeight: 'bold',
+    fontSize: height * 0.024,
+    color: '#FFFFFF',
   },
-
-
-
 });
-
 export default HomePage;
