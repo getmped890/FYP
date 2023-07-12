0f276ae8cab36c18a367e29eb8d5bbb95cf722be
qixian0622 <97826360+qixian0622@users.noreply.github.com>
2023-07-12 11:31:32 +0800

    implement generate attendance list

--- Jr_Academy/screens/Teacher/Attendance/GenerateAlist.js
+++ Jr_Academy/screens/Teacher/Attendance/GenerateAlist.js
@@ -1,30 +1,96 @@
 import React, { useState } from 'react';
-import {DatePicker} from 'react-native-date-picker';
-import {Text, Button, View} from 'react';
-import { Ionicons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
-import { StyleSheet } from 'react-native';
-
+import { View, Text, Button, FlatList, TextInput, StyleSheet } from 'react-native';
 
 const GenerateAlist = () => {
 
-    const [date, setDate] = useState(new Date())
+  //create array, when call set attendancelist, it passed the value back to attendancelist variable
+  const [attendanceList, setAttendanceList] = useState([]);
 
-    return (
-        <View>
-            <Text>Select Date to view attendance list</Text>
+  const generateAttendanceList = () => {
+    // get data from database
+    const exampleAttendanceData = [
+      { name: 'Navindran', status: 'Present' },
+      { name: 'Chow Xuhua', status: 'Late' },
+      { name: 'Qixian', status: 'Absent' },
+      { name: 'Guardiola', status: 'Late' },
+      { name: 'Hazard', status: 'Present' },
+      { name: 'Rashford', status: 'Late' },
+      { name: 'Mbappe', status: 'Absent' },
+    ];
+    setAttendanceList(exampleAttendanceData);
+  };
 
-            <DatePicker date={date} onDateChange={setDate} />
-            <Button title="Search" onPress={() => setOpen(true)} />
-            
+ 
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Input Date"
+        />
+        <Button color="black" title="Search"  />
+      </View>
+      <Button  color="black" title="Generate Attendance List" onPress={generateAttendanceList} />
+      {attendanceList.length > 0 ? (
+        <View style={styles.studentlistTable}>
+            <FlatList
+            data={attendanceList}
+                keyExtractor={(item) => item.name}
+            renderItem={({ item }) => (
+                <View style={styles.itemContainer}>
+                <Text style={styles.studentName}>{item.name}</Text>
+                <Text style={styles.studentStatus}>{item.status}</Text>
+                </View>
+            )} 
+             />
         </View>
-    );
+        //else 
+      ) : (
+        <Text style={styles.nodatafound}>No attendance data found.</Text>
+      )}
+    </View>
+  );
 };
 
-
 const styles = StyleSheet.create({
+  container: {
+    paddingTop:50,
+    flex: 1,
+    padding: 16,
+  },
+ 
+  nodatafound:{
+    paddingTop:20,
+  },
+  studentlistTable:{
+    paddingTop:30,
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  searchInput: {
+    flex: 1,
+    marginRight: 8,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    borderWidth: 1,
+    borderColor: 'gray',
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  studentName: {
+    flex: 1,
+    marginRight: 8,
+  },
+  studentStatus: {
+    fontWeight: 'bold',
+  },
+});
 
-
-  });
-  
 export default GenerateAlist;
