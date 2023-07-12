776c5179f9c3ca1a98436e7ac38d26a13fbe2374
qixian0622 <97826360+qixian0622@users.noreply.github.com>
2023-07-12 14:39:01 +0800

    attendance record page

---
+++ Jr_Academy/screens/Teacher/Attendance/AttendanceRecord.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { Alert } from 'react-native';
+
+const AttendanceRecord = () => {
+  const [selectedAttendance, setSelectedAttendance] = useState({});
+
+  const UpdateAttendance = () => {
+    //navigate to setting page
+    Alert.alert('Attendance Updated!');
+  };
+  const studentList = [
+    { id: '1', name: 'Navindran' },
+    { id: '2', name: 'Chow Xuhua' },
+    { id: '3', name: 'Qixian' },
+    { id: '4', name: 'Guardiola' },
+    { id: '5', name: 'Hazard' },
+    { id: '6', name: 'Rashford' },
+    { id: '7', name: 'Mbappe' },
+  ];
+
+  
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.studentlistTable}>
+        <FlatList
+          data={studentList}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+
+            <View style={styles.itemContainer}>
+
+              <Text style={styles.studentName}>{item.name}</Text>
+
+            </View>
+          )}
+        />
+      </View>
+      <Button color="black" title="Update Attendance" onPress={UpdateAttendance}/>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+    flex: 1,
+    padding: 16,
+  },
+  studentlistTable: {
+    paddingTop: 30,
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  studentName: {
+    flex: 1,
+    marginRight: 8,
+  },
+  attendancePicker: {
+    width: 150,
+  },
+});
+
+export default AttendanceRecord;
