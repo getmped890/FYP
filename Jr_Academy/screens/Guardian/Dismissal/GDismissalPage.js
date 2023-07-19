00df3cb08cafd3444055a7830f5631b5f8f30f6c
qixian0622 <97826360+qixian0622@users.noreply.github.com>
2023-07-19 22:59:35 +0800

    update guardian dismissal page

--- Jr_Academy/screens/Guardian/Dismissal/GDismissalPage.js
+++ Jr_Academy/screens/Guardian/Dismissal/GDismissalPage.js
@@ -5,30 +5,62 @@ const DismissalPage = () => {
   return (
     <View style={styles.container}>
       <View style={styles.background}>
-        <View style={styles.scrollContainer}>
-          <Text style={styles.headerText}>Dismissal Time:</Text>
-          <ScrollView horizontal={false} contentContainerStyle={styles.scrollContent}>
+
+      <View style={styles.dismissalTimeContainer}>
+          <Text style={styles.headerText}>Class Dismissal Time:</Text>
+          <ScrollView horizontal={true} contentContainerStyle={styles.scrollContent}>
             <View style={[styles.card, styles.cardElevated]}>
-              <Text style={styles.classText}>CSIT203</Text>
-              <Text style={styles.classText}>6:30pm</Text>
+              <Text style={styles.classText}>Class: CSIT203</Text>
+              <Text style={styles.classText}>Date: 26th Jun 2023</Text>
+              <Text style={styles.classText}>Dismissal Time: 3:30pm - 6:30pm</Text>
             </View>
             <View style={[styles.card, styles.cardElevated]}>
-              <Text style={styles.classText}>CSIT503</Text>
-              <Text style={styles.classText}>3:30pm</Text>
+              <Text style={styles.classText}>Class: CSIT203</Text>
+              <Text style={styles.classText}>Date: 26th Jun 2023</Text>
+              <Text style={styles.classText}>Dismissal Time: 3:30pm - 6:30pm</Text>
             </View>
             <View style={[styles.card, styles.cardElevated]}>
-              <Text style={styles.classText}>CSIT123</Text>
-              <Text style={styles.classText}>2:30pm</Text>
-            </View>
-            <View style={[styles.card, styles.cardElevated]}>
-              <Text style={styles.classText}>CSIT323</Text>
-              <Text style={styles.classText}>4:30pm</Text>
-            </View>
-            <View style={[styles.card, styles.cardElevated]}>
-              <Text style={styles.classText}>CSIT999</Text>
-              <Text style={styles.classText}>10:30pm</Text>
+              <Text style={styles.classText}>Class: CSIT203</Text>
+              <Text style={styles.classText}>Date: 26th Jun 2023</Text>
+              <Text style={styles.classText}>Dismissal Time: 3:30pm - 6:30pm</Text> 
             </View>
           </ScrollView>
+        </View>
+
+        <View style={styles.ccaTimeContainer}>
+          <Text style={styles.headerText}>CCA Dismissal Time:</Text>
+          <ScrollView horizontal={true} contentContainerStyle={styles.scrollContent}>
+            <View style={[styles.card, styles.cardElevated]}>
+              <Text style={styles.classText}>Badminton</Text>
+              <Text style={styles.classText}>Date: 26th Jun 2023</Text>
+              <Text style={styles.classText}>Dismissal Time: 3:30pm - 6:30pm</Text>
+            </View>
+            <View style={[styles.card, styles.cardElevated]}>
+              <Text style={styles.classText}>Football</Text>
+              <Text style={styles.classText}>Date: 26th Jun 2023</Text>
+              <Text style={styles.classText}>Dismissal Time: 3:30pm - 6:30pm</Text>
+            </View>
+            <View style={[styles.card, styles.cardElevated]}>
+              <Text style={styles.classText}>Class: Dancing</Text>
+              <Text style={styles.classText}>Date: 26th Jun 2023</Text>
+              <Text style={styles.classText}>Dismissal Time: 3:30pm - 6:30pm</Text> 
+            </View>
+          </ScrollView>
+        </View>
+
+        <View style={styles.reminderContainer}>
+        <Text style={styles.headerText}>Dismissal Reminder:</Text>
+
+          <ScrollView horizontal={true} contentContainerStyle={styles.scrollContent}>
+
+            <View style={[styles.card, styles.cardElevated]}>
+                  <Text style={styles.classText}>Dismissal Reminder:</Text>
+                  <Text style={styles.classText}></Text>
+                  <Text style={styles.classText}>Child Name: James</Text>
+                  <Text style={styles.classText}>Your child will be dismissed in an hour</Text>
+            </View>
+          </ScrollView>
+
         </View>
         
       </View>
@@ -50,8 +82,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    width: width * 0.65,
+    height: height * 0.16,
     width: width * 0.60,
-    height: height * 0.12,
+    height: height * 0.15,
     borderRadius: 4,
     margin: width * 0.05,
     backgroundColor: '#FFFFFF',
@@ -63,12 +97,25 @@ const styles = StyleSheet.create({
       height: 1,
     },
   },
-  scrollContainer: {
-    paddingTop: height * 0.1,
-    paddingBottom: height * 0.1,
+  dismissalTimeContainer: {
+    paddingTop: height * 0.05,
   },
- 
- 
+
+  ccaTimeContainer:{
+    paddingTop: height * 0.03,
+    paddingBottom:0,
+  },
+
+  reminderContainer:{
+    paddingTop: height * 0.03,
+  },
+
+  welcomeText: {
+    fontSize: height * 0.03,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    color: '#FFFFFF',
+  },
   headerText: {
     fontSize: height * 0.03,
     fontWeight: 'bold',
@@ -77,30 +124,12 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
   },
   classText: {
-    fontSize: height * 0.023,
+    fontSize: height * 0.018,
     color: '#1DC1B1',
   },
   scrollContent: {
     alignItems: 'center',
   },
-  endClassButton: {
-    position: 'absolute',
-    bottom: height * 0.05,
-    right: width * 0.03,
-    width: width * 0.2,
-    height: width * 0.14,
-    borderRadius: width * 0.1,
-    backgroundColor: '#FFFFFF',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  endClassText: {
-    position: 'absolute',
-    bottom: height * 0.01,
-    right: width * 0.03,
-    fontWeight: 'bold',
-    fontSize: height * 0.024,
-    color: '#FFFFFF',
-  },
+
 });
 export default DismissalPage;
