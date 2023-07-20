28e4e7299a83623d20a9e2ddd1b106e32bcaecae
qixian0622 <97826360+qixian0622@users.noreply.github.com>
2023-07-20 15:19:04 +0800

    sa register user , ensure all field are filled

--- Jr_Academy/screens/SchoolAdmin/RegisterUser/SARegisterUser.js
+++ Jr_Academy/screens/SchoolAdmin/RegisterUser/SARegisterUser.js
@@ -1,98 +1,115 @@
-import React, { useState, useLayoutEffect } from 'react';
-import {Alert, View, Text , TextInput, Button, StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import { Alert, View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import BackgroundColor from '../../Genericscreens/BackgroundSetting/BackgroundColor';
 import { Picker } from '@react-native-picker/picker';
 
 const SARegisterProfile = () => {
-
   const navigation = useNavigation();
+
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [email, setEmail] = useState('');
+  const [contact, setContact] = useState('');
+  const [role, setRole] = useState('teacher');
+
+  const isFormValid = () => {
+    return name.trim() !== '' && address.trim() !== '' && email.trim() !== '' && contact.trim() !== '';
+  };
+
+  const handleRoleChange = (value) => {
+    setRole(value);
+  };
+
+  const clearFields = () => {
+    setName('');
+    setAddress('');
+    setEmail('');
+    setContact('');
+    setRole('teacher');
+  };
+
   const SARegisterSubmitButtonClick = () => {
-    Alert.alert('New User Created!');
+      Alert.alert('New User Created!');
+      clearFields(); // clear all field value if form is submmited
+   
   };
 
   const SACancelButton = () => {
     navigation.navigate('Home');
   };
 
-  const [role, setRole] = useState('teacher');
-  const [status, setStatus] = useState('active');
-
-  const handleRoleChange = (value) => {
-    setRole(value);
-  };
-
-  const handleStatusChange = (value) => {
-    setStatus(value);
-  };
- 
   return (
     <BackgroundColor>
-    <View style={styles.container}>
-      <View style={styles.background}>
+      <View style={styles.container}>
+        <View style={styles.background}>
+          <Text style={styles.label}>Name:</Text>
+          <TextInput
+            style={styles.textInput}
+            value={name}
+            onChangeText={(text) => setName(text)}
+          />
 
-        <Text style={styles.label}>Name:</Text>
-        <TextInput
-          style={styles.textInput}
-          secureTextEntry
-        />
+          <Text style={styles.label}>Address:</Text>
+          <TextInput
+            style={styles.textInput}
+            value={address}
+            onChangeText={(text) => setAddress(text)}
+          />
 
-        <Text style={styles.label}>Address:</Text>
-        <TextInput
-          style={styles.textInput}
-   
-        />
-        
-        <Text style={styles.label}>Email:</Text>
-        <TextInput
-          style={styles.textInput}
-        />
-        <Text style={styles.label}>Contact:</Text>
-        <TextInput
-          style={styles.textInput}
-          
-        />
+          <Text style={styles.label}>Email:</Text>
+          <TextInput
+            style={styles.textInput}
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
 
-      <View style={styles.inputContainer}>
-          <Text style={styles.label}>Role:</Text>
-          <View style={styles.pickerstyle}>
-          <Picker
-            selectedValue={role}
-            onValueChange={(value) => handleRoleChange(value)}
-            style={styles.picker}
-          >
-            <Picker.Item label="Teacher" value="teacher" />
-            <Picker.Item label="Student" value="student" />
-            <Picker.Item label="CCA Coach" value="cca coach" />
-            <Picker.Item label="Guardian" value="guardian" />
-          </Picker>
+          <Text style={styles.label}>Contact:</Text>
+          <TextInput
+            style={styles.textInput}
+            value={contact}
+            onChangeText={(text) => setContact(text)}
+          />
+
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Role:</Text>
+            <View style={styles.pickerstyle}>
+              <Picker
+                selectedValue={role}
+                onValueChange={(value) => handleRoleChange(value)}
+                style={styles.picker}
+              >
+                <Picker.Item label="Teacher" value="teacher" />
+                <Picker.Item label="Student" value="student" />
+                <Picker.Item label="CCA Coach" value="cca coach" />
+                <Picker.Item label="Guardian" value="guardian" />
+              </Picker>
+            </View>
+          </View>
+
+          <View style={styles.buttonContainer}>
+            <Button
+              title="Submit"
+              onPress={SARegisterSubmitButtonClick}
+              color="black"
+              style={styles.submitbutton}
+              disabled={!isFormValid()} // Disable the button if all fileds are not filled in 
+            />
+          </View>
+
+          <View style={styles.buttonContainer}>
+            <Button
+              title="Cancel"
+              onPress={SACancelButton}
+              color="black"
+              style={styles.submitbutton}
+            />
           </View>
         </View>
-
-      <View style={styles.buttonContainer}>
-        <Button
-          title="Submit"
-          onPress={SARegisterSubmitButtonClick}
-          color="black"
-          style={styles.submitbutton}
-        />
-      </View>     
-      <View style={styles.buttonContainer}>
-        <Button
-          title="Cancel"
-          onPress={SACancelButton}
-          color="black"
-          style={styles.submitbutton}
-        />
-      </View>  
-              
-
       </View>
-    </View>
     </BackgroundColor>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
@@ -120,14 +137,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginBottom: 20,
   },
-  pickerstyle:{
+  pickerstyle: {
     borderColor: 'black',
     borderWidth: 1,
     borderRadius: 5,
     marginBottom: 5,
-    
-
-  }
+  },
 });
 
 export default SARegisterProfile;
