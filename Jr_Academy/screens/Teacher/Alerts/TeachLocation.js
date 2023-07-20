9ef9a6582be141975fbe1874f6498107eb853781
qixian0622 <97826360+qixian0622@users.noreply.github.com>
2023-07-20 12:19:33 +0800

    update teacher location

--- Jr_Academy/screens/Teacher/Alerts/TeachLocation.js
+++ Jr_Academy/screens/Teacher/Alerts/TeachLocation.js
@@ -2,56 +2,72 @@ import React from 'react';
 import { View, Text, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import TeachAlerts from './TeachAlerts';
 import BackgroundColor from '../../Genericscreens/BackgroundSetting/BackgroundColor';
+import MapView, { Marker } from 'react-native-maps';
 
-
-const TeachLocation = () => {
-
-     //use for navigating/redirect to other page
+const GLocation = () => {
+  //use for navigating/redirect to other page
   const navigation = useNavigation();
 
   const BackAlert = () => {
     //navigate to GPS Page
     navigation.goBack();
-
   };
 
- return (
-  <BackgroundColor>
+  return (
+    <BackgroundColor>
+      
 
-    <View>
-       <Text style={styles.backText}>Back</Text>       
-        <TouchableOpacity style={styles.backButton}>
-        <Ionicons name="chevron-back-outline" size={30} onPress={BackAlert}/>
-      </TouchableOpacity>
-    </View>
+      {/* map view for child location tracking  */}
+      <MapView
+        style={styles.map}
+        initialRegion={{
+            latitude: 3.1537,
+            longitude: 101.7143,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+
+        }}
+      >
+
+        
+        <View>
+        <Text style={styles.backText}>Back</Text>
+        <TouchableOpacity style={styles.backButton} onPress={BackAlert}>
+          <Ionicons name="chevron-back-outline" size={30} />
+        </TouchableOpacity>
+      
+
+
+        <Marker
+          coordinate={{ latitude: 3.1537,
+            longitude: 101.7143,}}  />
+
+</View>
+      </MapView>
     </BackgroundColor>
+  );
+};
 
- );};
-
-
- const styles = StyleSheet.create({
-
-
-    backButton: {
-
-        position: 'absolute',
-        top: 80,
-        left: 30,
-        zIndex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-
-    backText:{
-        position: 'relative',
-        top:50,
-        left:30,
-        fontSize:15,
-      fontWeight: 'bold',
-    },
-  
+const styles = StyleSheet.create({
+  backButton: {
+    position: 'absolute',
+    top: 80,
+    left: 30,
+    zIndex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  backText: {
+    position: 'relative',
+    top: 50,
+    left: 30,
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  map: {
+    flex: 1,
+  },
 });
-  
-export default TeachLocation;
+
+export default GLocation;
