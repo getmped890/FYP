fbc1ebd72d3dd448ee93079bb45490fed5378aa3
qixian0622 <97826360+qixian0622@users.noreply.github.com>
2023-07-20 12:20:00 +0800

    teacher location page

--- Jr_Academy/screens/Teacher/Alerts/TeachLocation.js
+++ Jr_Academy/screens/Teacher/Alerts/TeachLocation.js
@@ -5,7 +5,7 @@ import { useNavigation } from '@react-navigation/native';
 import BackgroundColor from '../../Genericscreens/BackgroundSetting/BackgroundColor';
 import MapView, { Marker } from 'react-native-maps';
 
-const GLocation = () => {
+const TeachLocation = () => {
   //use for navigating/redirect to other page
   const navigation = useNavigation();
 
@@ -70,4 +70,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default GLocation;
+export default TeachLocation;
