a89081fdb2f01af05c1d72f36e1820646231fb73
qixian0622 <97826360+qixian0622@users.noreply.github.com>
2023-07-20 15:37:53 +0800

    validation on sa update own profile page

--- Jr_Academy/screens/SchoolAdmin/SAProfile/SAUpdateOwnProfile.js
+++ Jr_Academy/screens/SchoolAdmin/SAProfile/SAUpdateOwnProfile.js
@@ -1,9 +1,9 @@
-import React, { useState, useLayoutEffect } from 'react';
+import React, { useState } from 'react';
 import { View, TouchableOpacity, TextInput, StyleSheet, Text, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import BackgroundColor from '../../Genericscreens/BackgroundSetting/BackgroundColor';
-const SAUpdateProfile = () => {
-  
+
+const SAUpdateOwnProfile = () => {
   const navigation = useNavigation();
 
   const handleCancelButtonClick = () => {
@@ -14,80 +14,78 @@ const SAUpdateProfile = () => {
     navigation.navigate('SAProfile');
   };
 
-  // Use useLayoutEffect to set the options for hiding the header
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerShown: false,
-    });
-  }, [navigation]);
+  // Use useState to manage the input values
+  const [name, setName] = useState('James Milner');
+  const [address, setAddress] = useState('45 yishun kim');
+  const [email, setEmail] = useState('jdf@gmail.com');
+  const [contact, setContact] = useState('91234567');
+
+  const isFormValid = () => {
+    return name.trim() !== '' && address.trim() !== '' && email.trim() !== '' && contact.trim() !== '';
+  };
 
   return (
     <BackgroundColor>
- 
-    <View style={styles.container}>
-      
-       <View style={styles.inputContainer}>
-        <Text style={styles.label}>Name:</Text>
-        <TextInput
-          style={styles.textInput}
-          value="James Milner"
-          editable={false}
-        />
-      </View>
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Name:</Text>
+          <TextInput
+            style={styles.textInput}
+            value={name}
+            onChangeText={(text) => setName(text)}
+          />
+        </View>
 
-      
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Address:</Text>
-        <TextInput
-          style={styles.textInput}
-          value="45 yishun kim"
-          editable={false}
-        />
-      </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Address:</Text>
+          <TextInput
+            style={styles.textInput}
+            value={address}
+            onChangeText={(text) => setAddress(text)}
+          />
+        </View>
 
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Email:</Text>
-        <TextInput
-          style={styles.textInput}
-          value="jdf@gmail.com"
-          editable={true}
-        />
-      </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Email:</Text>
+          <TextInput
+            style={styles.textInput}
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+        </View>
 
-     
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Contact:</Text>
+          <TextInput
+            style={styles.textInput}
+            value={contact}
+            onChangeText={(text) => setContact(text)}
+          />
+        </View>
 
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Contact:</Text>
-        <TextInput
-          style={styles.textInput}
-          value="91234567"
-          editable={true}
-        />
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Update"
+            onPress={handleSubmitButtonClick}
+            color="black"
+            style={styles.submitbutton}
+            disabled={!isFormValid()} // Disable the button if all fields are not filled in
+          />
+          <Button
+            title="Cancel"
+            onPress={handleCancelButtonClick}
+            color="black"
+            style={styles.cancelbutton}
+          />
+        </View>
       </View>
-
-      <View style={styles.buttonContainer}>
-        <Button
-          title="Update"
-          onPress={handleSubmitButtonClick}
-          color="black"
-          style={styles.submitbutton}
-        />
-        <Button
-          title="Cancel"
-          onPress={handleCancelButtonClick}
-          color="black"
-          style={styles.cancelbutton}
-        />
-      </View>
-    </View>
     </BackgroundColor>
   );
 };
 
 const styles = StyleSheet.create({
-
-  newUsertext:{
-    fontSize:20,
+  newUsertext: {
+    fontSize: 20,
   },
   container: {
     flex: 1,
@@ -113,10 +111,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 20,
-    marginRight: 20, 
+    marginRight: 20,
   },
-
-  
 });
 
-export default SAUpdateProfile;
+export default SAUpdateOwnProfile;
